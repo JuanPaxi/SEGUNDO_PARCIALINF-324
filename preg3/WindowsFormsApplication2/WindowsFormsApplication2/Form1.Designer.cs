3bc13896c3a0dfc429a4b39c8a64ee98ff5ac9fa
JuanPaxi <49133333+JuanPaxi@users.noreply.github.com>
2023-12-15 09:02:33 +0100

    SEG PARCIAL

---
+++ preg3/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.cargar_img = new System.Windows.Forms.Button();
+            this.pintar_col = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(188, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 397);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(39, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // cargar_img
+            // 
+            this.cargar_img.Location = new System.Drawing.Point(39, 29);
+            this.cargar_img.Name = "cargar_img";
+            this.cargar_img.Size = new System.Drawing.Size(100, 23);
+            this.cargar_img.TabIndex = 4;
+            this.cargar_img.Text = "Cargar Imagen";
+            this.cargar_img.UseVisualStyleBackColor = true;
+            this.cargar_img.Click += new System.EventHandler(this.cargar_img_Click);
+            // 
+            // pintar_col
+            // 
+            this.pintar_col.Location = new System.Drawing.Point(39, 162);
+            this.pintar_col.Name = "pintar_col";
+            this.pintar_col.Size = new System.Drawing.Size(100, 23);
+            this.pintar_col.TabIndex = 6;
+            this.pintar_col.Text = "Pintar Colores\r\n\r\n";
+            this.pintar_col.UseVisualStyleBackColor = true;
+            this.pintar_col.Click += new System.EventHandler(this.pintar_col_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 413);
+            this.Controls.Add(this.pintar_col);
+            this.Controls.Add(this.cargar_img);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button cargar_img;
+        private System.Windows.Forms.Button pintar_col;
+    }
+}
+
---
+++ preg3/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Linq;//not
+using System.Text;
+using System.Threading.Tasks;//not
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        string connectionString = "Data Source=(local);Initial Catalog=inf324;Integrated Security=True;";
+        int Rm, Gm, Bm;
+        
+        int Rt, Gt, Bt;
+        int L = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+        private void cargar_img_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = string.Empty;
+
+            openFileDialog1.Filter = "Archivos JPG|*.JPG|Archivos BMP|*.bmp";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != string.Empty)
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            c = bmp.GetPixel(e.X, e.Y);
+            
+            textBox1.Text = c.R.ToString();
+            textBox2.Text = c.G.ToString();
+            textBox3.Text = c.B.ToString();
+
+            
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string query = "INSERT INTO color VALUES (@R, @G, @B)";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@R", textBox1.Text);
+            command.Parameters.AddWithValue("@G", textBox2.Text);
+            command.Parameters.AddWithValue("@B", textBox3.Text);
+            
+            command.ExecuteNonQuery();
+            Console.WriteLine("Dato insertado correctamente");
+            connection.Close();
+        }
+
+        private void insertar_col_Click(object sender, EventArgs e)
+        {
+
+            /*
+             * -------------------USAS ESTA CONEXION IGUAL FUNCIONA (En todas Partes)-------
+             * -------------------PARA QUE NO SEA TODO IGUAL
+             *
+            SqlConnection con = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            con.ConnectionString ="Data Source= (local) ;Initial Catalog=color;Integrated Security=True;";
+            con.Open();
+            cmd.Connection = con;
+             
+             //consulta ejeplo
+            cmd.CommandText = "insert into color values(123,555,888)";
+             
+            cmd.ExecuteNonQuery();
+            con.Close();
+             
+            */
+            
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string query = "INSERT INTO color VALUES (@R, @G, @B)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@R", textBox1.Text);
+            command.Parameters.AddWithValue("@G", textBox2.Text);
+            command.Parameters.AddWithValue("@B", textBox3.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+             
+        }
+
+        private void pintar_col_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+
+            List<Tuple<int, int, int>> listaColores = ObtenerElementosDesdeBaseDeDatos();
+            foreach (var color in listaColores)
+            {
+                Console.WriteLine("R:" +color.Item1+"G:" +color.Item2 +" B:"+color.Item3);
+                int rr = color.Item1;
+                int gg = color.Item2;
+                int bb = color.Item3;
+
+                for (int i = 0; i < bmp.Width - L; i = i + L)
+                {
+                    for (int j = 0; j < bmp.Height - L; j = j + L)
+                    {
+                        Rt = 0; Gt = 0; Bt = 0;
+                        for (int o = i; o < i + L; o++)
+                            for (int p = j; p < j + L; p++)
+                            {
+                                c = bmp.GetPixel(o, p);
+                                Rt += c.R;
+                                Gt += c.G;
+                                Bt += c.B;
+                            }
+                        Rt = Rt / (L * L); Gt = Gt / (L * L); Bt = Bt / (L * L);
+                        if (((rr - L < Rt) && (Rt < rr + L))
+                            && ((gg - L < Gt) && (Gt < gg + L))
+                            && ((bb - L < Bt) && (Bt < bb + L)))
+                        {
+                            for (int o = i; o < i + L; o++)
+                                for (int p = j; p < j + L; p++)
+                                {
+                                    bmp2.SetPixel(o, p, Color.FromArgb(Math.Abs(rr - 255), Math.Abs(gg - 255), Math.Abs(bb - 255)));
+                                }
+                        }
+                        else
+                        {
+                            for (int o = i; o < i + L; o++)
+                                for (int p = j; p < j + L; p++)
+                                {
+                                    c = bmp.GetPixel(o, p);
+                                    bmp2.SetPixel(o, p, Color.FromArgb(c.R, c.G, c.B));
+                                }
+                        }
+                    }
+                }
+                bmp = bmp2;
+
+            }
+            pictureBox1.Image = bmp2;
+        }
+
+        private List<Tuple<int, int, int>> ObtenerElementosDesdeBaseDeDatos()
+        {
+            List<Tuple<int, int, int>> colorList = new List<Tuple<int, int, int>>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT r, g, b FROM color";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int r = reader.GetInt32(0);
+                            int g = reader.GetInt32(1);
+                            int b = reader.GetInt32(2);
+
+                            // Agregar la tupla a la lista
+                            colorList.Add(Tuple.Create(r, g, b));
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return colorList;
+        }
+
+    }
+}
